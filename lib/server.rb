9d69f7154566ba82add8192bec0b731b102e3f5e
Daniel Schauenberg <d@unwiredcouch.com>
2010-06-06 23:10:25 +0200

    add basic sinatra server

---
+++ lib/server.rb
@@ -0,0 +1,44 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'mustache/sinatra'
+require 'lib/data'
+
+
+class Thingnatra
+
+  class Server < Sinatra::Base
+    register Mustache::Sinatra
+
+    base = File.dirname(__FILE__) + "/.."
+
+    set :logging, :true
+    set :root, File.dirname(__FILE__)
+    set :public, "#{base}/static"
+    require base + '/views/layout'
+
+    set :mustache, {
+      :views     => "#{base}/views/",
+      :templates => "#{base}/templates/",
+      :namespace => Thingnatra
+    }
+
+    def initialize(*args)
+      super
+      @data = Thingnatra::Data.new('Database.xml')
+    end
+
+    # index page
+    get '/?' do
+      mustache :index
+    end
+
+    #list of today items
+    get '/today/?' do
+      @tasks = @data.get_todays_items
+      mustache :today
+    end
+
+
+
+  end
+end
