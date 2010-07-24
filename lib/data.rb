c38f6caec0519d921e82163b98ad8af1c9b6078b
Daniel Schauenberg <d@unwiredcouch.com>
2010-07-24 11:53:39 +0200

    make file check in initializer

--- lib/data.rb
+++ lib/data.rb
@@ -4,22 +4,22 @@ class Thingnatra
   class Data
 
     def initialize(things_data)
-      @things_data = things_data
-      self.init_data(things_data)
+      if File.exists? things_data
+        @things_data = things_data
+        self.init_data(things_data)
+      else
+        None
+      end
     end
 
     def init_data(db_url)
-      if File.exists? db_url
-        @things = Things.new(:database => db_url)
-      end
-      if @things
-        @box_map = {
-                      :inbox => @things.inbox,
-                      :today => @things.today,
-                      :next => @things.next,
-                      :scheduled => @things.scheduled,
-                  }
-      end
+      @things = Things.new(:database => db_url)
+      @box_map = {
+                    :inbox => @things.inbox,
+                    :today => @things.today,
+                    :next => @things.next,
+                    :scheduled => @things.scheduled,
+                }
     end
 
     def get_items_for(box)
