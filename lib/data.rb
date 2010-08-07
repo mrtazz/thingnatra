6333b420d238c62e8cd13cded98ec6a9b1b434c6
Daniel Schauenberg <d@unwiredcouch.com>
2010-08-07 18:01:41 +0200

    move exception handling into init_data method

--- lib/data.rb
+++ lib/data.rb
@@ -6,22 +6,22 @@ class Thingnatra
   class Data
 
     def initialize(things_data)
-      if File.exists? things_data
         self.init_data(things_data)
-      else
-        raise Thingnatra::Exceptions::DatabaseFileError
-      end
     end
 
     def init_data(db_url)
-      @things = Things.new(:database => db_url)
-      @box_map = {
-                    :inbox => @things.inbox,
-                    :today => @things.today,
-                    :next => @things.next,
-                    :scheduled => @things.scheduled,
-                    :logbook => @things.logbook,
-                }
+      if File.exists? db_url
+        @things = Things.new(:database => db_url)
+        @box_map = {
+                      :inbox => @things.inbox,
+                      :today => @things.today,
+                      :next => @things.next,
+                      :scheduled => @things.scheduled,
+                      :logbook => @things.logbook,
+                  }
+      else
+        raise Thingnatra::Exceptions::DatabaseFileError
+      end
     end
 
     def get_items_for(box)
